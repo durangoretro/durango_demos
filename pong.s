9b1ae372049cf8ed1f901bc07e767c20bce58f6c
emiliollbb <emiliollbb@gmail.com>
2022-07-30 09:26:47 +0200

    Initial constants

--- pong.s
+++ pong.s
@@ -1,6 +1,31 @@
 ; -----------------------------------------------
+; 0 0000
+; 1 0001
+; 2 0010
+; 3 0011
+; 4 0100
+; 5 0101
+; 6 0110
+; 7 0111
+; 8 1000
+; 9 1001
+; a 1010
+; b 1011
+; c 1100
+; d 1101
+; e 1110
+; f 1111
 ; Constants
 ROM_START = $c000
+VIDEO_MODE = $df80
+HIRES = $80
+INVERT = $40
+SCREEN_0 = $00
+SCREEN_1 = $10
+SCREEN_2 = $20
+SCREEN_3 = $30
+RGB = $08
+LED = $04
 NEGRO = $00
 VERDE = $11
 ROJO = $22
@@ -34,15 +59,18 @@ _main:
 
 
 ; Set video mode
-; [HiRes Invert S1 S0    RGB LED NC NC]
-LDA #$3F
-STA $df80
-
+LDA #(RGB | SCREEN_3)
+STA VIDEO_MODE
+; Init video pointer
+LDA #$60
+STA VMEM_POINTER+1
+LDA #$00
+STA VMEM_POINTER
 
 ; Set coords
-LDA #$10
+LDA #10
 STA X_COORD
-LDA #$20
+LDA #20
 STA Y_COORD
 
 ; Set color
@@ -51,10 +79,6 @@ STA CURRENT_COLOR
 
 JSR convert_coords_to_mem
 
-LDA #$60
-STA VMEM_POINTER+1
-LDA #$00
-STA VMEM_POINTER
 
 ; Draw pixel
 LDA #$11
