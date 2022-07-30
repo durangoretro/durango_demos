61e4443fb465b368b54a935c06473b21cb45fedc
emiliollbb <emiliollbb@gmail.com>
2022-07-30 09:50:56 +0200

    Aux functions

--- pong.s
+++ pong.s
@@ -40,35 +40,36 @@ p1_begin = $00
 
 ; -- main method --
 _main:
+.(
+    ; Set video mode
+    LDA #(RGB | SCREEN_3)
+    STA VIDEO_MODE
+    ; Init video pointer
+    LDA #$60
+    STA VMEM_POINTER+1
+    LDA #$00
+    STA VMEM_POINTER
 
-; Set video mode
-LDA #(RGB | SCREEN_3)
-STA VIDEO_MODE
-; Init video pointer
-LDA #$60
-STA VMEM_POINTER+1
-LDA #$00
-STA VMEM_POINTER
+    ; Set coords
+    LDA #10
+    STA X_COORD
+    LDA #20
+    STA Y_COORD
 
-; Set coords
-LDA #10
-STA X_COORD
-LDA #20
-STA Y_COORD
+    ; Set color
+    LDA CIAN
+    STA CURRENT_COLOR
 
-; Set color
-LDA CIAN
-STA CURRENT_COLOR
-
-JSR convert_coords_to_mem
+    JSR _convert_coords_to_mem
 
 
-; Draw pixel
-LDA #$11
-STA CURRENT_COLOR
-STA (VMEM_POINTER), Y
+    ; Draw pixel
+    LDA #$11
+    STA CURRENT_COLOR
+    STA (VMEM_POINTER), Y
 
-end: JMP end
+    end: JMP end
+.)
 ; -- end main method --
 
 ; ------- FUNCTIONS --------------------------------
@@ -76,90 +77,126 @@ end: JMP end
 
 ; X_COORD, Y_COORD pixel coords
 ; VMEM_POINTER VMEM_POINTER+1 current video memory pointer
-convert_coords_to_mem:
-LDX #$00
-; Multiply y coord by 64 (64 bytes each row)
-LDA Y_COORD
-ASL
-; Also shift more sig byte
-TAY
-TXA
-ROL
-TAX
-TYA
-; Shift less sig byte
-ASL
-; Also shift more sig byte
-TAY
-TXA
-ROL
-TAX
-TYA
-; Shift less sig byte
-ASL
-; Also shift more sig byte
-TAY
-TXA
-ROL
-TAX
-TYA
-; Shift less sig byte
-ASL
-; Also shift more sig byte
-TAY
-TXA
-ROL
-TAX
-TYA
-; Shift less sig byte
-ASL
-; Also shift more sig byte
-TAY
-TXA
-ROL
-TAX
-TYA
-; Shift less sig byte
-ASL
-; Also shift more sig byte
-TAY
-TXA
-ROL
-TAX
-TYA
-; Shift less sig byte
-; Add to initial memory address, and save it
-CLC
-ADC VMEM_POINTER
-STA VMEM_POINTER
+_convert_coords_to_mem:
+.(
+    LDX #$00
+    ; Multiply y coord by 64 (64 bytes each row)
+    LDA Y_COORD
+    ASL
+    ; Also shift more sig byte
+    TAY
+    TXA
+    ROL
+    TAX
+    TYA
+    ; Shift less sig byte
+    ASL
+    ; Also shift more sig byte
+    TAY
+    TXA
+    ROL
+    TAX
+    TYA
+    ; Shift less sig byte
+    ASL
+    ; Also shift more sig byte
+    TAY
+    TXA
+    ROL
+    TAX
+    TYA
+    ; Shift less sig byte
+    ASL
+    ; Also shift more sig byte
+    TAY
+    TXA
+    ROL
+    TAX
+    TYA
+    ; Shift less sig byte
+    ASL
+    ; Also shift more sig byte
+    TAY
+    TXA
+    ROL
+    TAX
+    TYA
+    ; Shift less sig byte
+    ASL
+    ; Also shift more sig byte
+    TAY
+    TXA
+    ROL
+    TAX
+    TYA
+    ; Shift less sig byte
+    ; Add to initial memory address, and save it
+    CLC
+    ADC VMEM_POINTER
+    STA VMEM_POINTER
 
-; If overflow, add one to more sig byte
-BCC conv_coor_mem_01
-INX
-conv_coor_mem_01:
-; Add calculated offset to VMEM_POINTER+1 (more sig)
-TXA
-CLC
-ADC VMEM_POINTER+1
-STA VMEM_POINTER+1
+    ; If overflow, add one to more sig byte
+    BCC conv_coor_mem_01
+    INX
+    conv_coor_mem_01:
+    ; Add calculated offset to VMEM_POINTER+1 (more sig)
+    TXA
+    CLC
+    ADC VMEM_POINTER+1
+    STA VMEM_POINTER+1
 
-; Calculate X coord
-; Divide x coord by 2 (2 pixel each byte)
-LDA X_COORD
-LSR
-; Add to memory address
-CLC
-ADC VMEM_POINTER
-; Store in video memory position
-STA VMEM_POINTER
-; If overflow, increment left byte
-BCC conv_coor_mem_02
-INC VMEM_POINTER+1
-conv_coor_mem_02:
-RTS
+    ; Calculate X coord
+    ; Divide x coord by 2 (2 pixel each byte)
+    LDA X_COORD
+    LSR
+    ; Add to memory address
+    CLC
+    ADC VMEM_POINTER
+    ; Store in video memory position
+    STA VMEM_POINTER
+    ; If overflow, increment left byte
+    BCC conv_coor_mem_02
+    INC VMEM_POINTER+1
+    conv_coor_mem_02:
+    RTS
+.)
 ; --- end convert_coords_to_mem ---
 
+_init:
+.(
+    LDX #$FF  ; Initialize stack pointer to $01FF
+    TXS
+    CLD       ; Clear decimal mode
+    JSR _main
+.)
 
+_stop:
+.(
+    .byte $DB
+.)
+
+_nmi_int:
+.(
+    BRK
+    RTI
+.)
+
+_irq_int:
+.(
+    ; Filter interrupt
+    PHX         ; Save X register contents to stack
+    TSX         ; Transfer stack pointer to X
+    PHA         ; Save accumulator contents to stack
+    LDA $102,X  ; Load status register contents
+    AND #$10    ; Isolate B status bit
+    BNE _stop   ; If B = 1, BRK detected
+    ; Actual interrupt code
+    NOP
+    ; Return from interrupt
+    PLA                    ; Restore accumulator contents
+    PLX                    ; Restore X register contents
+    RTI                    ; Return from all IRQ interrupts
+.)
 
 
 ; --------------------------------------------------
@@ -169,7 +206,7 @@ RTS
 
 ; Set initial PC
 * = $fffa
-    .word end ; NMI vector
-    .word _main ; Reset vector
-    .word end ; IRQ/BRK vector
+    .word _nmi_int ; NMI vector
+    .word _init ; Reset vector
+    .word _irq_int ; IRQ/BRK vector
     
