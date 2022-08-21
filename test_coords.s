d9cb59eb381b92cb239d74603f4d8a246f6e7ffc
emiliollbb <emiliollbb@gmail.com>
2022-08-21 08:52:54 +0200

    Test loop types

--- test_coords.s
+++ test_coords.s
@@ -41,26 +41,21 @@ AZUR = $cc
 CIAN = $dd
 FUCSIA = $ee
 BLANCO = $ff
-
 ; -- Functions args pointers --
 VMEM_POINTER = $10 ; $11
+TILE_TO_DRAW = $12 ; $13
+MAP_TO_DRAW = $14 ; 15
 X_COORD = $16
 Y_COORD = $17
-SQ_WIDTH = $08
-SQ_HEIGHT = $09
-CURRENT_COLOR = $06
+SQ_WIDTH = $18
+SQ_HEIGHT = $19
+DRAW_BUFFER = $1a
+CURRENT_COLOR = $1b
+TEMP1 = $1c
+TEMP2 = $1d
 
-; -- Global Game constants --
-PADDLE_WIDTH = 6
-PADDLE_HEIGHT =32
-BACKGROUND = ROSITA
-; -- Global Game vars pointers --
-p1_vertical_x = 00
-p1_vertical_y = 01
-p2_vertical_x = 02
-p2_vertical_y = 03
 
-; == 16K ROM. FIRST 8K BLOCK ==
+
 *=$c000
 begin:
 
@@ -69,119 +64,43 @@ begin:
 LDA #$3c
 STA $df80
 
-LDA #5
-STA Y_COORD
-STA VMEM_POINTER+1
+LDA #SERIAL_HEX
+STA SERIAL_PORT+1
 
-LDA #118
-STA X_COORD
+; [08][07][06][05][04][03][02][01]
+.(
+LDY #08
+loop:
+STY SERIAL_PORT
+DEY
+BNE loop
+.)
 
+LDA #SERIAL_ASCII
+STA SERIAL_PORT+1
+LDA #$0a
+STA SERIAL_PORT
 
+LDA #SERIAL_HEX
+STA SERIAL_PORT+1
 
-
-LDA #PADDLE_WIDTH
-STA SQ_WIDTH
-LDA #PADDLE_HEIGHT
-STA SQ_HEIGHT
-LDA #BLANCO
-STA CURRENT_COLOR
-
-;JSR debug3
-JSR _convert_coords_to_mem
-JSR debug2
-JSR _draw_square
-
-;JSR debug3
-JSR _convert_coords_to_mem
-JSR debug2
-JSR _draw_square
+; [07][06][05][04][03][02][01][00]
+.(
+LDY #08
+DEY
+loop:
+STY SERIAL_PORT
+DEY
+BPL loop
+.)
 
 end: JMP end
 
 
-; Converts x,y coord into memory pointer.
-; X_COORD, Y_COORD pixel coords
-; VMEM_POINTER VMEM_POINTER+1 current video memory pointer
-_convert_coords_to_mem:
-.(
-    ; Clear X reg
-    LDX #$00
-    ; Clear VMEM_POINTER
-    STX VMEM_POINTER
-    ; Multiply y coord by 64 (64 bytes each row)
-    LDA Y_COORD
-    LSR
-    STA VMEM_POINTER+1
-    ROR VMEM_POINTER
-    ; Sencond shift
-    LSR VMEM_POINTER+1
-    ROR VMEM_POINTER
-    
-    ; Add base memory address
-    CLC
-    LDA VMEM_POINTER+1
-    ADC #$60
-    STA VMEM_POINTER+1
-    LDA VMEM_POINTER
-    ADC #$00
-    STA VMEM_POINTER
-    
-    ; Calculate X coord
-    ; Divide x coord by 2 (2 pixel each byte)
-    LDA X_COORD
-    LSR
-    ; Add to memory address
-    CLC
-    ADC VMEM_POINTER
-    STA VMEM_POINTER
-    LDA VMEM_POINTER+1
-    ADC #$00
-    STA VMEM_POINTER+1
-    
-    RTS
-.)
-; --- end convert_coords_to_mem ---
-
-; Draw square
-; X_COORD, Y_COORD, SQ_WIDTH, SQ_HEIGHT
-; VMEM_POINTER VMEM_POINTER+1 final video memory pointer
-_draw_square:
-.(
-	JSR _convert_coords_to_mem
-	; Load height in x
-	LDX SQ_HEIGHT
-	paint_row:
-	; Divide width by 2
-	LDA SQ_WIDTH
-	LSR
-	; Store it in Y
-	TAY
-	; Load current color
-	LDA CURRENT_COLOR
-	; Draw as many pixels as Y register says
-	paint:
-	STA (VMEM_POINTER), Y
-	DEY
-	BNE paint:
-	STA (VMEM_POINTER), Y
-	; Next row
-;	LDA VMEM_POINTER
-;	CLC
-;	ADC #$40
-;	STA VMEM_POINTER
-;	BCC skip_upper
-;	INC VMEM_POINTER+1
-;	skip_upper:
-;	DEX
-;	BNE paint_row
-	RTS
-.)
-; --- end draw_square
-
 debug:
 .(
     PHA
-    LDA #SERIAL_BINARY
+    LDA #SERIAL_HEX
     STA SERIAL_PORT+1
     LDA VMEM_POINTER+1
     STA SERIAL_PORT
