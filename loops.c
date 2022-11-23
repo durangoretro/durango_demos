006b36c8e26a012ece777c8be3e6b8fae0fd494d
Emilio Lopez Berenguer <emilio@emiliollbb.net>
2022-11-23 10:43:01 +0100

    Test loops

--- loops.c
+++ loops.c
@@ -11,10 +11,20 @@ void test_do_while(void);
 void test_for(void);
 void test_while(void);
 
+struct point {
+    unsigned char x, y;
+};
 
-unsigned char i;
+
+unsigned char i, j;
+struct point p[10];
 
 int main(void){    
+    p[0].x=1;
+    p[0].y=2;
+    
+    p[1].x=3;
+    p[1].y=4;
     return 0;
 }
 
@@ -22,6 +32,9 @@ void test_if(void) {
     if(i==0) {
         consoleLogHex(i);
     }
+    else {
+        consoleLogDecimal(i);
+    }   
 }
 
 void test_ifnot(void) {
@@ -37,9 +50,11 @@ void test_ifnumber(void) {
 }
 
 void test_do_while() {
+    i=0;
     do {
         consoleLogHex(i);
-    }while(i!=0);
+        i++;
+    }while(i!=10);
 }
 
 void test_for() {
