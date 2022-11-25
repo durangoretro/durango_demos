58001914a0dbe97ab0f37473ff30d8f69c9f5893
Emilio Lopez Berenguer <emilio@emiliollbb.net>
2022-11-25 12:11:09 +0100

    New lib examples

--- newlib.c
+++ newlib.c
@@ -1,7 +1,9 @@
 #include <durango.h>
 
 int main(void){    
-    fillScreen(RED);
+    fillScreen(WHITE);
+    fillRect(10, 10, 100, 50, RED);
+    drawLine(10, 10, 110, 60, BLACK);
     return 0;
 }
 
