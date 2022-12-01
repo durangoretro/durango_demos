b5b11cf7d5250c454d6b97d7316c14c3a91036a9
Emilio <emiliollbb@gmail.com>
2022-12-01 20:35:59 +0100

    Test keyboard

--- newlib.c
+++ newlib.c
@@ -1,10 +1,14 @@
 #include <durango.h>
 
+char key;
 int main(void){    
     fillScreen(WHITE);
     fillRect(10, 10, 100, 50, RED);
     drawLine(10, 10, 110, 60, BLACK);
     drawCircle(50, 40, 30, GREEN);
+	key=getChar();
+	consoleLogHex(0x00);
+	consoleLogHex(key);
     return 0;
 }
 
