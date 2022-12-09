5a7187371d54ab3dd45d9400dcbde65473d26aa1
Emilio Lopez Berenguer <emilio@emiliollbb.net>
2022-12-09 15:10:35 +0100

    Test conio

--- newlib.c
+++ newlib.c
@@ -6,6 +6,9 @@ int main(void){
     fillRect(10, 10, 100, 50, RED);
     drawLine(10, 10, 110, 60, BLACK);
     drawCircle(50, 40, 30, GREEN);
+    
+    printstr("Hello World!");
+    
 	do{
         k=getChar();
         consoleLogChar(k);
