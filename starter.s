6c9280b3dee593bf3b0dde603fa004476757c738
Emilio <emiliollbb@gmail.com>
2023-01-02 15:41:04 +0100

    Add irq

--- starter.s
+++ starter.s
@@ -8,6 +8,8 @@ SEI
 CLD
 LDX #$FF
 TXS
+LDA #$00
+STA $dfa0
 .)
 
 ; Set video mode
@@ -70,18 +72,16 @@ wait: BCC wait
 LDA #%00011000
 STA $df80
 LDA #$11
+LDX #$20
+STX $01
 LDY #$00
-STY $01
-LDX #$02
-STX $00
+STY $00
 loop:
 STA ($00),Y
 INY
 BNE loop
 INC $01
 BPL loop
-STA $00
-STA $01
 CLC
 wait: BCC wait
 .)
@@ -91,10 +91,10 @@ wait: BCC wait
 LDA #%00101000
 STA $df80
 LDA #$22
+LDX #$40
+STX $01
 LDY #$00
-STY $01
-LDX #$02
-STX $00
+STY $00
 loop:
 STA ($00),Y
 INY
@@ -112,10 +112,10 @@ wait: BCC wait
 LDA #%00111000
 STA $df80
 LDA #$33
+LDX #$60
+STX $01
 LDY #$00
-STY $01
-LDX #$02
-STX $00
+STY $00
 loop:
 STA ($00),Y
 INY
@@ -129,6 +129,7 @@ wait: BCC wait
 .)
 
 ; 2. Play tone
+.(
 LDX #$ff     ; Duration
 dur_loop:
 STX $dfb0
@@ -138,9 +139,24 @@ DEY
 BNE loop
 DEX
 BPL dur_loop
+.)
 
 ; Test IRQ
-
+.(
+LDA #%00111100
+STA $df80
+LDY #$60
+STY $01
+LDX #$00
+STX $00 
+LDA #$44
+CLI
+LDY #$01
+STY $dfa0
+wait:
+LDX $01
+BPL wait
+.)
 
 
 
@@ -160,6 +176,17 @@ nmi:
 	PLA
         end:
 	RTI
+.)
+
+int:
+.(
+
+loop:
+STA ($00),Y
+INY
+BNE loop
+INC $01
+BPL loop
 .)
 
 ; === FILLING ===
