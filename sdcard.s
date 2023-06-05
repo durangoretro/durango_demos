4e018adcd37dd9d5ece2d26b33365c87150842a8
Emilio Lopez Berenguer <emilio@emiliollbb.net>
2023-06-05 12:51:17 +0200

    SD card demo

---
+++ sdcard.s
@@ -0,0 +1,233 @@
+*=$c000
+
+;------ DXHEAD------------------------------------------------------
+
+; 8 bytes
+.byt $00
+.byt "dX"
+.byt "****"
+.byt $0d
+
+; 222 bytes
+; TITLE_COMMENT[
+.byt $00, $00
+.byt "################################################"
+.byt "##################################################"
+.byt "##################################################"
+.byt "##################################################"
+.byt "######################";]
+
+
+; 18 bytes
+;DCLIB_COMMIT[
+.byt "LLLLLLLL"
+;]
+;MAIN_COMMIT[
+.byt "MMMMMMMM"
+;]
+;VERSION[
+.byt "VV"
+;]
+
+; 8 bytes
+;TIME[
+.byt "TT"
+;]
+;DATE[
+.byt "DD"
+;]
+;FILEZISE[
+.byt $00,$40,$00,$00
+;]
+;---------------------------------------------------------------
+
+begin:
+
+; Initialize 6502    
+SEI ; Disable interrupts
+CLD ; Clear decimal mode
+LDX #$FF ; Initialize stack pointer to $01FF
+TXS
+
+; Set video mode
+; [HiRes Invert S1 S0    RGB LED NC NC]
+LDA #$3F
+STA $df80
+
+; Clean screen
+LDA #$00
+LDX #$60
+STX $01
+LDY #$00
+STY $00
+loopcs:
+STA ($00), Y
+INY
+BNE loopcs
+INC $01
+BPL loopcs
+
+
+; DCLIB CONSTANTS ----------------------------
+VMEM_POINTER = $10 ; $11
+DATA_POINTER = $12 ; $13
+RESOURCE_POINTER = $14 ; $15
+BACKGROUND_POINTER = $16; $17
+RANDOM_SEED = $18; $19
+COLOUR = $1A
+PAPER = $1B
+X_COORD = $1C
+Y_COORD = $1D
+X2_COORD = $1E
+Y2_COORD = $1F
+X3_COORD = $20
+Y3_COORD = $21
+X4_COORD = $22
+Y4_COORD = $23
+HEIGHT = $24
+WIDTH = $25
+HEIGHT2 = $26
+WIDTH2 = $27
+TEMP1 = $28
+TEMP2 = $29
+TEMP3 = $2A
+TEMP4 = $2B
+TEMP5 = $2C
+TEMP6 = $2D
+TEMP7 = $2E
+TEMP8 = $2F
+
+
+; ****************************************************************
+; We are ready for actual work -----------------------------------
+
+mosi=TEMP1
+arg=TEMP2; - TEMP5
+miso=TEMP6
+crc=TEMP7
+tmpba=TEMP8
+
+
+
+
+
+; ***********************************
+; *** hardware-specific SD driver ***
+; ***********************************
+; SD interface definitions
+#define	SD_CLK		%00000001
+#define	SD_MOSI		%00000010
+#define	SD_CS		%00000100
+#define	SD_MISO		%10000000
+#define	IOCart		$DFC0
+
+; *** send data in A, return received data in A *** nominally ~4.4 kiB/s
+spi_tr:
+	STA mosi
+	LDY #8					; x = 8;
+	LDA #SD_CLK
+	TRB IOCart				; digitalWrite(SCK, 0); (13t)
+    tr_l:						; while (x)
+		ASL mosi
+		LDA IOCart
+		AND #SD_MOSI^$FF
+		BCC mosi_set
+			ORA #SD_MOSI
+    mosi_set:
+		STA IOCart			; digitalWrite(MOSI, data & 128); data <<= 1;
+		INC IOCart			; digitalWrite(SCK, 1);	** assume SD_CLK  is   1 **
+		ASL					; in <<= 1;				** assume SD_MISO is $80 **
+		ROL miso			; if(digitalRead(MISO)) in++;
+		DEC IOCart			; digitalWrite(SCK, 0);	** assume SD_CLK  is   1 **
+		DEY					; x--;
+		BNE tr_l			; (worst case, 8*43 = 344t)
+	LDA miso				; return in; (total including call overhead = 372t, ~242 µs)
+	RTS
+
+
+; *** enable card transfer ***
+cs_enable:
+	LDA #$FF
+	JSR spi_tr				; SPI_transfer(0xFF);
+	LDA #SD_CS
+	TRB IOCart				; CS_ENABLE();
+	LDA #$FF
+	JMP spi_tr				; SPI_transfer(0xFF); ...and return
+
+; *** disable card transfer ***
+cs_disable:
+	LDA #$FF
+	JSR spi_tr				; SPI_transfer(0xFF);
+	LDA #SD_CS
+	TSB IOCart				; CS_DISABLE();
+	LDA #$FF
+	JMP spi_tr				; SPI_transfer(0xFF); ...and return
+
+; ***************************
+; *** standard SD library ***
+; ***************************
+
+; *** send command in A to card *** arg.l, crc.b
+sd_cmd:
+; send command header
+	ORA #$40
+	JSR spi_tr				; SPI_transfer(cmd|0x40);
+; send argument
+	LDA arg
+	JSR spi_tr				; SPI_transfer((u_int8_t)(arg >> 24));
+	LDA arg+1
+	JSR spi_tr				; SPI_transfer((u_int8_t)(arg >> 16));
+	LDA arg+2
+	JSR spi_tr				; SPI_transfer((u_int8_t)(arg >> 8));
+	LDA arg+3
+	JSR spi_tr				; SPI_transfer((u_int8_t)(arg));
+; send CRC
+	LDA crc
+	ORA #1
+	JMP spi_tr				; SPI_transfer(crc|0x01); ...and return
+
+; *** *** special version of the above, in case SDSC is byte-addressed, CMD17 and CMD24 only *** ***
+ba_cmd:
+; send command header
+	ORA #$40
+	JSR spi_tr				; SPI_transfer(cmd|0x40);
+; precompute byte-addressed sector
+	LDA arg+3
+	ASL
+	STA tmpba+1
+	LDA arg+2
+	ROL
+	STA tmpba
+	LDA arg+1
+	ROL						; A holds MSB
+; send argument
+	JSR spi_tr
+	LDA tmpba
+	JSR spi_tr
+	LDA tmpba+1
+	JSR spi_tr
+	LDA #0					; always zero as 512 bytes/sector
+	JSR spi_tr
+; send CRC
+	LDA crc
+	ORA #1
+	JMP spi_tr				; SPI_transfer(crc|0x01); ...and return
+
+
+
+
+
+
+
+; ****************************************************************
+; End of actual work -------------------------------------
+; Tail
+; Dev-Cart JMP at $FFE1
+.dsb    $ffe1-*, $ff
+JMP($FFFC)
+
+; Vectors
+.dsb    $fffa-*, $ff    ; filling
+.word begin
+.word begin
+.word begin
